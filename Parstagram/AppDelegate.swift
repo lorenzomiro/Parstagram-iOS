214014c33109104354e8620e61d540b6c2cf20f6
lorenzomiro <lorenzo.miro.sd@gmail.com>
2022-02-26 22:03:31 -0800

    Login/sign up implemented

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // --- Copy this only
+            
+            let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "5V44YysLyCqfY5KEdVNL4LUX1CEH2eR7jUtkbcbk" // <- UPDATE
+                    $0.clientKey = "eASxzdlMoiMw3F2QDFjIQO0FYLD2z3Y4V5Jr4rtz" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,88 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Miro on 2/26/22.
+//
+
+import UIKit
+
+import Parse
+
+class LoginViewController: UIViewController {
+
+    //create fields for user + password
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        let username = usernameField.text!
+        
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, Error) in
+            
+            if user != nil { //perform login segue if login is valid
+                
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                
+            } else { //throw error if login is unsuccessful
+                
+                print("Error: \(Error?.localizedDescription)")
+                
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        let user = PFUser()
+        
+        //get username + password fields for logging in
+        
+        user.username = usernameField.text
+        
+        user.password = passwordField.text
+        
+        user.password = "myPassword"
+        
+        user.signUpInBackground { (success, error) in
+            
+            if success {
+                
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                
+            } else { //print out error
+                
+                print("Error: \(error?.localizedDescription)")
+                
+            }
+            
+        }
+
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
