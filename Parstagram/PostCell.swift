59c5f8afd9aaa475c25a1d4cbaa1530d6d11da17
lorenzomiro <lorenzo.miro.sd@gmail.com>
2022-02-27 01:05:22 -0800

    Need help with querying posts/part 6 - hooking up username

--- Parstagram/PostCell.swift
+++ Parstagram/PostCell.swift
@@ -13,6 +13,8 @@ class PostCell: UITableViewCell {
     
     @IBOutlet weak var photoView: UIImageView!
     
+    @IBOutlet weak var usernameLabel: UILabel!
+    
     @IBOutlet weak var captionLabel: UILabel!
 
     override func awakeFromNib() {
